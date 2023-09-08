a1486307c547e8440a375674b624f61a1987db58
Ian Perry <Ianperry99@gmail.com>
2023-09-08 01:31:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -73,3 +73,18 @@ sudo systemctl stop nmbd
 sudo systemctl stop smbd
 sudo systemctl disable nmbd
 sudo systemctl disable smbd
+
+# Download the powershell '.tar.gz' archive
+curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/powershell-7.3.6-linux-arm64.tar.gz
+
+# Create the target folder where powershell will be placed
+sudo mkdir -p /opt/microsoft/powershell/7
+
+# Expand powershell to the target folder
+sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+
+# Set execute permissions
+sudo chmod +x /opt/microsoft/powershell/7/pwsh
+
+# Create the symbolic link that points to pwsh
+sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
