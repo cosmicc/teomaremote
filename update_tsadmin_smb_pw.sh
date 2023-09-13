1915f8ef8926fd271c063cd8c1bb42a659a663ba
ip <ianperry99@gmail.com>
2023-09-13 00:03:36 -0400

    update

--- update_tsadmin_smb_pw.sh
+++ update_tsadmin_smb_pw.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Error: This script must be run as root."
+    exit 1
+fi
 
 echo -n "tsadmin user "
-sudo smbpasswd -a tsadmin
+smbpasswd -a tsadmin
